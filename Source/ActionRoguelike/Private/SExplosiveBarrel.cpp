afb8e12514505a763d662ce47487f7646d8be581
ExcellentKAKA <59327000+ExcellentKAKA@users.noreply.github.com>
2023-04-05 15:48:49 +0800

    Supply File

---
+++ Source/ActionRoguelike/Private/SExplosiveBarrel.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SExplosiveBarrel.h"
+
+#include "PhysicsEngine/RadialForceComponent.h"
+
+// Sets default values
+ASExplosiveBarrel::ASExplosiveBarrel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshComp");
+	MeshComp->SetSimulatePhysics(true);
+	MeshComp->SetCollisionProfileName(UCollisionProfile::PhysicsActor_ProfileName);
+	RootComponent = MeshComp;
+
+	RadialForceComp = CreateDefaultSubobject<URadialForceComponent>(TEXT("RadialComp"));
+	RadialForceComp->SetupAttachment(RootComponent);
+	RadialForceComp->Radius = 750.0f;
+	RadialForceComp->ImpulseStrength = 3000.f;
+	RadialForceComp->bImpulseVelChange = true;
+
+	
+	
+}
+
+void ASExplosiveBarrel::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	MeshComp->OnComponentHit.AddDynamic(this,&ASExplosiveBarrel::OnMeshHit);
+}
+
+// Called when the game starts or when spawned
+void ASExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASExplosiveBarrel::OnMeshHit( UPrimitiveComponent* HitComponent,
+	AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	RadialForceComp->FireImpulse();
+
+	//Debug Message
+
+	UE_LOG(LogTemp,Warning,TEXT("Other Actor is %s at time %f"), *GetNameSafe(OtherActor), GetWorld()->TimeSeconds);
+
+	
+	FString HitLocation = FString::Printf(TEXT("Hit at %s"),*Hit.ImpactPoint.ToString());
+	DrawDebugString(GetWorld(),Hit.Location,HitLocation,nullptr,FColor::Cyan,4.0f);
+}
+
+// Called every frame
+void ASExplosiveBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
